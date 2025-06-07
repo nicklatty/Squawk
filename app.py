ce8bfb742ab70f637cb965ab3f34236683226b05
Nick Latty <nicklatty@icloud.com>
2025-06-07 21:31:03 +1200

    Test webhook connection

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from flask import Flask, request
+import subprocess
 import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -10,10 +13,26 @@ def home():
         "photos/" + filename
         for filename in os.listdir(photo_folder)
         if filename.lower().endswith((".jpg", ".jpeg", ".png"))
-           and not filename.endswith('_remix.jpg') and not filename.endswith('_remix.png')
+        and not filename.endswith('_remix.jpg') and not filename.endswith('_remix.png')
     ]
     photo_files.sort()
     return render_template("about.html", photos=photo_files)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+app = Flask(__name__)
+
+
+@app.route('/update', methods=['POST'])
+def webhook():
+    try:
+        subprocess.Popen(['git', 'pull'], cwd='/home/squawkLYT/mysite')
+        # Reload web app
+        subprocess.Popen(
+            ['touch', '/var/www/squawklyt_pythonanywhere_com_wsgi.py'])
+        return 'Update triggered successfully.', 200
+    except Exception as e:
+        return f'Error: {str(e)}', 500
