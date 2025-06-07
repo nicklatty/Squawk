6e991619b2bf23f0ac008aaba65a4cee0070b7ea
Nick Latty <nicklatty@icloud.com>
2025-06-07 22:59:08 +1200

    Meaningful message about what you changed

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request
-import subprocess
 import os
-from flask import Flask, render_template
+import subprocess
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -19,20 +18,21 @@ def home():
     return render_template("about.html", photos=photo_files)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-app = Flask(__name__)
-
-
 @app.route('/update', methods=['POST'])
 def webhook():
     try:
-        subprocess.Popen(['git', 'pull'], cwd='/home/squawkLYT/mysite')
-        # Reload web app
+        subprocess.Popen(['git', 'pull'], cwd='/home/squawkLYT/Squawk')
         subprocess.Popen(
             ['touch', '/var/www/squawklyt_pythonanywhere_com_wsgi.py'])
         return 'Update triggered successfully.', 200
     except Exception as e:
         return f'Error: {str(e)}', 500
+
+
+@app.route("/ping")
+def ping():
+    return "pong"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
